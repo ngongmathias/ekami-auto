12c883ad564a8d1abe7f2b67f5a711f158d16350
ngongmathias <mathiasngongngai@gmail.com>
2025-11-23 03:14:04 +0200

    feat: Add Tools link to navigation menu

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -20,6 +20,7 @@ export default function Header() {
     { path: '/compare', label: 'Compare' },
     { path: '/repairs', label: t('nav.repairs') },
     { path: '/sell', label: t('nav.sell') },
+    { path: '/tools', label: 'Tools' },
     { path: '/blog', label: t('nav.blog') },
   ];
 
